0ab34074e9cf2643aaf913f36ae87fce72b92b21
Harsh8196 <harshmashruwala@gmail.com>
2021-09-29 00:44:23 +0530

    fixing provider

--- src/script/web3_.js
+++ src/script/web3_.js
@@ -1,4 +1,5 @@
 const Web3 = require('web3');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
  
 let web3;
  
@@ -6,6 +7,13 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   // We are in the browser and metamask is running.
   window.ethereum.request({ method: "eth_requestAccounts" });
   web3= new Web3(window.web3.currentProvider);
+} else {
+  const provider = new HDWalletProvider(
+    'comic hero roof harsh main make please ill slush away spice slab',
+    'https://api.s0.b.hmny.io'
+  );
+  
+  web3 = new Web3(provider);
 }
 
 export default web3
